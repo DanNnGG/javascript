2145bef0ee894e83672a3ced3d3351adaa796be5
darilodsg <139563258+darilodsg@users.noreply.github.com>
2024-04-16 16:15:19 -0300

    aulta16

---
+++ aula13/teste.js
@@ -0,0 +1,30 @@
+var c = 1
+do {
+    console.log(`Número ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+/*
+
+var c = 1
+while (c <= 6){
+    console.log(`Número ${c}`)
+    c++
+}
+
+*/
+
+
+
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+
